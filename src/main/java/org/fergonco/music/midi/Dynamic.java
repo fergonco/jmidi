63ff9a1032d58f9e6b347c580923e663243c3011
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-08-30 15:46:51 +0200

    dynamics as enum

--- src/main/java/org/fergonco/music/midi/Dynamic.java
+++ src/main/java/org/fergonco/music/midi/Dynamic.java
@@ -1,15 +1,26 @@
 package org.fergonco.music.midi;
 
-public interface Dynamic {
-	int PPPP = 8;
-	int PPP = 20;
-	int PP = 31;
-	int P = 42;
-	int MP = 53;
-	int MF = 64;
-	int F = 80;
-	int FF = 96;
-	int FFF = 112;
-	int FFFF = 127;
+public enum Dynamic {
+	MUTE(0), PPPP(8), PPP(20), PP(31), P(42), MP(53), MF(64), F(80), FF(96), FFF(112), FFFF(127);
+
+	private int level;
+
+	Dynamic(int level) {
+		this.level = level;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public Dynamic louder() {
+		Dynamic[] dynamics = Dynamic.values();
+		for (int j = 0; j < dynamics.length; j++) {
+			if (dynamics[j] == this && j + 1 < dynamics.length) {
+				return dynamics[j + 1];
+			}
+		}
+		return this;
+	}
 
 }
